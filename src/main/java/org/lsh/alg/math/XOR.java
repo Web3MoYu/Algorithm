daa60b6ff2b1dc4b2a9dd26171dd674b51e3c97b
lsh <3040114965@qq.com>
2024-09-11 14:48:26 +0800

    xor

---
+++ src/main/java/org/lsh/alg/math/XOR.java
@@ -0,0 +1,11 @@
+package org.lsh.alg.math;
+
+/**
+ * 异或运算，相同为0，不同为1相当于无进位相加
+ * 性质：
+ * 0^N = N, N^N = 0
+ * 满足交换律和结合律
+ * 所以交换两个数的值可以写成 a = a^b b = a^b a = a^b
+ */
+public class XOR {
+}
--- src/main/java/org/lsh/alg/util/AlgUtils.java
+++ src/main/java/org/lsh/alg/util/AlgUtils.java
@@ -2,9 +2,13 @@ package org.lsh.alg.util;
 
 public class AlgUtils {
 
+    /**
+     * 交换二者，前提两个数的内存不是一块，否则为0
+     */
     public static void swap(int[] arr, int i, int j) {
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+
     }
 }
