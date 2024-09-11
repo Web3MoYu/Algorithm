c17ed56b7a9b8230d0bfec6b089afeba6e644ff5
lsh <3040114965@qq.com>
2024-09-11 16:09:26 +0800

    binaryFind

--- src/main/java/org/lsh/alg/find/BinaryFind.java
+++ src/main/java/org/lsh/alg/find/BinaryFind.java
@@ -1,15 +1,27 @@
 package org.lsh.alg.find;
 
-import java.util.Arrays;
-
 public class BinaryFind {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(Arrays.toString(arr));
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(binaryFind(arr, 11));
     }
 
-    public static int binaryFind(int[] arr) {
+    public static int binaryFind(int[] arr, int num) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (arr[mid] == num) {
+                return mid;
+            }
+            if (arr[mid] > num) {
+                right = mid - 1;
+            }
+            if (arr[mid] < num) {
+                left = mid + 1;
+            }
+        }
         return -1;
     }
 }
