49f98ba4825f408130d29cf6a62962ebe12f1840
lsh <3040114965@qq.com>
2024-09-11 15:06:52 +0800

    xor

--- src/main/java/org/lsh/alg/math/XOR.java
+++ src/main/java/org/lsh/alg/math/XOR.java
@@ -8,4 +8,29 @@ package org.lsh.alg.math;
  * 所以交换两个数的值可以写成 a = a^b b = a^b a = a^b
  */
 public class XOR {
+
+    /**
+     * 有一堆数，有两个是奇数次，其他是偶数次，找到奇数次的数
+     * @param arr
+     */
+    public static void xor(int[] arr) {
+        int xor = 0, xor1 = 0;
+        for (int num : arr) {
+            xor ^= num;
+        }
+        /**
+         * 101101
+         * 010011
+         */
+        // 获得最右侧的1
+        int rightOne = xor & (~xor + 1);
+        for (int num : arr) {
+            // 找到a或b中的一个
+            if ((num & rightOne) == 0) {
+                xor1 ^= num;
+            }
+        }
+
+        System.out.println(xor + " " + (xor ^ xor1));
+    }
 }
